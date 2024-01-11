d404181467b72231b2c02d6f715ae7ded869f7e9
root <Chigozirim Uwaezuoke>
2024-01-11 02:20:15 -0800

    annottions

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Type-annotated function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """func that takes
+    two str variables and returns their concatenation"""
+    return str1 + str2
